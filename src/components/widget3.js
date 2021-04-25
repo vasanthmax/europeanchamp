153e81089f3bde0fac56575fbaff3c4966d77af9
vasanthmaxwell <vasanthgta@gmail.com>
2021-04-25 13:59:20 +0530

    ics build completed

--- src/components/widget3.js
+++ src/components/widget3.js
@@ -4,7 +4,6 @@ import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import { Link } from 'react-router-dom';
 import { createEvent } from 'ics';
-import { writeFileSync, writeFile } from 'fs';
 
 const Widget3 = () => {
   const calendar =
